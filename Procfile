38e7aa1cbce91b7fac83f68894c6c91a421fcad2
chrisRagira <christopherragira@gmail.com>
2023-11-20 07:42:54 +0300

    fairy dust

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bet.wsgi
+web: gunicorn bet.wsgi --log-file -
--- bet/settings.py
+++ bet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
