67c6ef0474422679a20e605c2965a0dcc73e2735
chrisRagira <christopherragira@gmail.com>
2023-11-30 11:40:02 +0300

    lord

--- main/site_scheduler/site_updater.py
+++ main/site_scheduler/site_updater.py
@@ -5,5 +5,5 @@ from main.scrapper import fetch_matches
 def start():
     print('started')
     scheduler = BackgroundScheduler()
-    scheduler.add_job(fetch_matches, "interval", minutes=5, id="matches_001", replace_existing=True)
+    # scheduler.add_job(fetch_matches, "interval", minutes=5, id="matches_001", replace_existing=True)
     scheduler.start()
