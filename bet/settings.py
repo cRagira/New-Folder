6710491e9924fb6561cfbea0ca70be1f9fb673c3
iRagira <princeguy01@gmail.com>
2024-01-03 18:33:12 +0300

    styles still

--- bet/settings.py
+++ bet/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'bet.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default':dj_database_url.config(default=DATABASE_URL, conn_max_age=5000),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':dj_database_url.config(default=DATABASE_URL, conn_max_age=5000),
 }
 
 CSRF_TRUSTED_ORIGINS=[
